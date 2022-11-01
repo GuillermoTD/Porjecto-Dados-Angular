3cd13c6cc2ca83641c1af41fc65200ee2c7c32d6
GuillermoTD <88951508+GuillermoTD@users.noreply.github.com>
2022-11-01 16:06:34 -0400

    primer branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  dadoIzquierda = '../../img/dice1.png';
+  dadoDerecha = '../../img/dice4.png';
+  numero1: number;
+  numero2: number;
+
+  tirarDado(): void {}
 }
